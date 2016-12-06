55c800f4b894939d34bddd8ae1ab5e01973be6be
Jun Hao <lifeyellow4.gmail.com>
2016-12-05 22:56:27 -0500

    customize update

--- web/scripts/customize.js
+++ web/scripts/customize.js
@@ -5,7 +5,10 @@
 var game;
 var customizeMap;
 var defaultMap;
+var monsterStack;
+var money = 100;
 var size;
+var stackIndex = 19;
 var window_width = window.innerWidth;
 var window_height = window.innerHeight;
 
@@ -47,46 +50,6 @@ function MapBaseListener(){
             numberOfTowers++;
             money-=100;
         }
-    }else if(selectedTower === 1){
-        if(money>100) {
-            this.param1.setImage(new towerObj(numberOfTowers, 'eyetower', size, 20, 500, game, Bullets277, this.param1.getPosition()[0], this.param1.getPosition()[1], enemyArray, 500, this.param1, false));
-            this.param1.isTowerSet = true;
-            towerArray.push(this.param1.getImage());
-            numberOfTowers++;
-            money-=100;
-        }
-    }else if(selectedTower === 2){
-        if(money>=150) {
-            this.param1.setImage(new towerObj(numberOfTowers, 'xueyoutower', size, 40, 1000, game, Bullets224, this.param1.getPosition()[0], this.param1.getPosition()[1], enemyArray, 300, this.param1, false));
-            this.param1.isTowerSet = true;
-            towerArray.push(this.param1.getImage());
-            numberOfTowers++;
-            money-=150;
-        }
-    }else if(selectedTower === 3) {
-        if(money>=80) {
-            this.param1.setImage(new towerObj(numberOfTowers, 'javatower', size, 100, 400, game, Bullets74, this.param1.getPosition()[0], this.param1.getPosition()[1], enemyArray, 800, this.param1, false));
-            this.param1.isTowerSet = true;
-            towerArray.push(this.param1.getImage());
-            numberOfTowers++;
-            money-=80;
-        }
-    }else if(selectedTower === 4){
-        if(money>=200) {
-            this.param1.setImage(new towerObj(numberOfTowers, 'fantower', size, 20, 400, game, windBullets, this.param1.getPosition()[0], this.param1.getPosition()[1], enemyArray, 800, this.param1, false));
-            this.param1.isTowerSet = true;
-            towerArray.push(this.param1.getImage());
-            numberOfTowers++;
-            money-=200;
-        }
-    }else if(selectedTower === 5){
-        if(money>=120) {
-            this.param1.setImage(new towerObj(numberOfTowers, 'geartower', size, 30, 3000, game, healingBullets, this.param1.getPosition()[0], this.param1.getPosition()[1], enemyArray, 4000, this.param1, true));
-            this.param1.isTowerSet = true;
-            towerArray.push(this.param1.getImage());
-            numberOfTowers++;
-            money-=120;
-        }
     }
 
     //******   add by Jun *****////
@@ -95,23 +58,30 @@ function MapBaseListener(){
     // 6 is start grid
     else if (selectedTower === 6){
 
-        this.param1 = new mapBase(this.param1.getPosition()[0], this.param1.getPosition()[1],game.add.image(this.param1.getPosition()[0], this.param1.getPosition()[1],'start'),'start');
+        this.param1 = new mapBase(this.param1.getPosition()[0], this.param1.getPosition()[1],game.add.image(this.param1.getPosition()[0], this.param1.getPosition()[1],'start'),'grid');
         this.param1.getImage().scale.setTo(size/10/100,size/10/100);
         this.param1.mapBaseBg.scale.setTo(size/10/100,size/10/100);
+        this.param1.getImage().inputEnabled = true;
+        this.param1.getImage().events.onInputDown.add(MapBaseListener, {param1: this.param1 , param2:this.param2});
         defaultMap[this.param2] = 5;
+
 
     }
     // 7 is path
     else if (selectedTower === 7){
-        this.param1 = new mapBase(this.param1.getPosition()[0], this.param1.getPosition()[1],game.add.image(this.param1.getPosition()[0], this.param1.getPosition()[1],'path'),'path');
+
+        this.param1 = new mapBase(this.param1.getPosition()[0], this.param1.getPosition()[1],game.add.image(this.param1.getPosition()[0], this.param1.getPosition()[1],'path'),'grid');
         this.param1.getImage().scale.setTo(size/10/100,size/10/100);
         this.param1.mapBaseBg.scale.setTo(size/10/100,size/10/100);
+        this.param1.getImage().inputEnabled = true;
+        this.param1.getImage().events.onInputDown.add(MapBaseListener, {param1: this.param1 , param2:this.param2});
         defaultMap[this.param2] = 1;
     }
 
     // 8 is end
 
     else if (selectedTower === 8){
+
         this.param1 = new mapBase(this.param1.getPosition()[0], this.param1.getPosition()[1],game.add.sprite(this.param1.getPosition()[0], this.param1.getPosition()[1],'home'),'grid');
         this.param1.getImage().frame = 1;
         this.param1.getImage().scale.setTo(size/10/400,size/10/400);
@@ -119,14 +89,29 @@ function MapBaseListener(){
         this.param1.getImage().animations.add('homeAnimation',[0,1,2,3,4,5,6,7,8,9],10,true);
         this.param1.getImage().animations.play('homeAnimation');
         home = this.param1.getImage();
+        this.param1.getImage().inputEnabled = true;
+        this.param1.getImage().events.onInputDown.add(MapBaseListener, {param1: this.param1 , param2:this.param2});
 
-        game.physics.enable(home, Phaser.Physics.ARCADE);
-        home.body.immovable = false;
+
+
         defaultMap[this.param2] = 4;
     }
 
+    else if (selectedTower === 9){
+
+        this.param1 = new mapBase(this.param1.getPosition()[0], this.param1.getPosition()[1],game.add.image(this.param1.getPosition()[0], this.param1.getPosition()[1],'grid'),'grid');
+        this.param1.getImage().scale.setTo(size/10/100,size/10/100);
+        this.param1.mapBaseBg.scale.setTo(size/10/100,size/10/100);
+        defaultMap[this.param2] = 0;
+        this.param1.getImage().inputEnabled = true;
+        this.param1.getImage().events.onInputDown.add(MapBaseListener, {param1: this.param1 , param2:this.param2});
+
+
+        console.log(defaultMap);
+
+    }
     //******   add by Jun *****////
-    console.log(money);
+
 }
 
 
@@ -135,6 +120,44 @@ function actionOnClick () {
     selectedTower = this.param1;
 
 }
+function saveAction(){
+    var content = JSON.stringify({Map : defaultMap, Monster: monsterStack, Money : money});
+
+    console.log(content);
+
+
+
+
+
+
+}
+
+
+
+function addMonster(){
+
+
+    if (this.monster ===0){
+        monsterStack.push(0);
+
+
+
+
+    }
+    else if (this.monster ===1){
+        monsterStack.push(1);
+    }
+    else if (this.monster ===2){
+        monsterStack.push(2);
+    }
+    else if (this.monster ===3){
+        monsterStack.push(3);
+    }
+    else if (this.monster ===4){
+        monsterStack.push(4);
+    }
+}
+
 
 
 window.onload = function() {
@@ -159,11 +182,19 @@ customizeScreen.prototype ={
         game.load.spritesheet('home', 'assets/path/home.png', 400, 400);
         game.load.image('path', 'assets/path/pathnew2.png');
         game.load.image('start', 'assets/path/start_stage.png');
+        game.load.image("blank",'assets/grid.png')
+
+        game.load.spritesheet('eye', 'assets/monsters/mob_mov_2.png', 300, 300, 5);
+        game.load.spritesheet('feiLian', 'assets/monsters/feiLian.png',600,600,7);
+        game.load.spritesheet('huoDou', 'assets/monsters/dou_ani.png',599,599,7);
+        game.load.spritesheet('hong', 'assets/monsters/hong_ani.png',599,599,7);
+        game.load.spritesheet('qiLin', 'assets/monsters/qilin_ani.png',599,599,7);
+        game.load.spritesheet('tie', 'assets/monsters/tie_ani.png',599,599,7);
 
     },
     create: function(){
         size  = Math.min(window.innerHeight,window.innerWidth);
-        var monsterStack = new Array();
+        monsterStack = new Array();
         customizeMap = new Array();
 
         defaultMap =[
@@ -194,23 +225,24 @@ customizeScreen.prototype ={
                     customizeMap[k].getImage().inputEnabled = true;
                     customizeMap[k].getImage().events.onInputDown.add(MapBaseListener, {param1: customizeMap[k] , param2:k});
 
-                }else if(gameIndexArray[k]===1){
+                }else if(defaultMap[k]===1){
 
 
-                    customizeMap[k] = new mapBase(j,i,game.add.image(j,i,'path'),'path');
+                    customizeMap[k] = new mapBase(j,i,game.add.image(j,i,'path'),'grid');
                     customizeMap[k].getImage().scale.setTo(size/10/100,size/10/100);
                     customizeMap[k].mapBaseBg.scale.setTo(size/10/100,size/10/100);
+                    customizeMap[k].getImage().inputEnabled = true;
+                    customizeMap[k].getImage().events.onInputDown.add(MapBaseListener, {param1: customizeMap[k] , param2:k});
+
+                }else if(defaultMap[k]===5){
 
 
-                }else if(gameIndexArray[k]===5){
-
-
-                    customizeMap[k] = new mapBase(j,i,game.add.image(j,i,'start'),'start');
+                    customizeMap[k] = new mapBase(j,i,game.add.image(j,i,'start'),'grid');
                     customizeMap[k].getImage().scale.setTo(size/10/100,size/10/100);
                     customizeMap[k].mapBaseBg.scale.setTo(size/10/100,size/10/100);
+                    customizeMap[k].getImage().events.onInputDown.add(MapBaseListener, {param1: customizeMap[k] , param2:k});
 
-
-                }else if(gameIndexArray[k]===4){
+                }else if(defaultMap[k]===4){
 
 
                     customizeMap[k] = new mapBase(j,i,game.add.sprite(j,i,'home'),'grid');
@@ -223,7 +255,8 @@ customizeScreen.prototype ={
 
                     game.physics.enable(home, Phaser.Physics.ARCADE);
                     home.body.immovable = false;
-
+                    customizeMap[k].getImage().inputEnabled = true;
+                    customizeMap[k].getImage().events.onInputDown.add(MapBaseListener, {param1: customizeMap[k] , param2:k});
 
                 }
                 k++;
@@ -233,17 +266,28 @@ customizeScreen.prototype ={
         var cstartButton = game.add.button(size/20*19, size/3, 'eyetowerButton', actionOnClick, {param1:6}, 0, 0, 0);
         var cpathButton = game.add.button(size/20*18, size/3, 'eyetowerButton', actionOnClick, {param1:7}, 0, 0, 0);
         var cendButton = game.add.button(size/20*17, size/3, 'eyetowerButton', actionOnClick, {param1:8}, 0, 0, 0);
+        var removeButton = game.add.button(size/20*16, size/3, 'eyetowerButton', actionOnClick, {param1:9}, 0, 0, 0);
 
-        save_label = game.add.text(w - 100, 20, 'Save map', { font: '24px Arial', fill: '#fff' });
-        save_label.inputEnabled = true;
-        save_label.events.onInputUp.add(function(){
-            defaultMap.toString();
-        });
+        var saveButton = game.add.button(size/20*30, size/3, 'eyetowerButton',saveAction);
+
+        var addFeilian = game.add.button(size/20*19, size/2, 'eyetowerButton',addMonster,{monster:0});
+        var addHon = game.add.button(size/20*24, size/2, 'eyetowerButton',addMonster,{monster:1});
+        var addHuodou = game.add.button(size/20*25, size/2, 'eyetowerButton',addMonster,{monster:2});
+        var addqilin = game.add.button(size/20*26, size/2, 'eyetowerButton',addMonster,{monster:3});
+        var addeye= game.add.button(size/20*27, size/2, 'eyetowerButton',addMonster,{monster:4});
+
+
+
         // textfield
         //monster array
 
 
-    }// create end
+    },// create end
+    update: function(){
+        if (addFeilian.input.onDown){
+            game.add.sprite(100,200,"feiLian");
+        }
+    }
 
 
 }
