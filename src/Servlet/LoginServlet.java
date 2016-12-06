6a2438732b23540c6a581285eabebf5c13d9da5b
haozhi1798 <haozhi1798@gmail.com>
2016-12-06 03:58:39 +0000

    old servlet

--- src/Servlet/LoginServlet.java
+++ src/Servlet/LoginServlet.java
@@ -1,12 +1,10 @@
 package Servlet;
 
 import Connector.JdbcConnector;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
@@ -14,6 +12,9 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Created by Rex on 10/26/16.
+ */
 
 public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -22,24 +23,17 @@ public class LoginServlet extends HttpServlet {
         response.setHeader("Access-Control-Allow-Origin","*");
 
         try (PrintWriter out = response.getWriter()) {
-            //get user info from gogole assign
             String Id = request.getParameter("id");
             String Name= request.getParameter("name");
-            int Level = -1;
-            if(!(Id.equalsIgnoreCase("Guest"))) {
-                //if user is not in the database, set level to 1
-                Level = 1;
-                // abtain user info from database
+            int Level = 1;
+            if(!(Id.equalsIgnoreCase("Guest"))){
                 String query = "select * from account where Id = '" + Id + "'";
                 ResultSet rs = JdbcConnector.excuteQuery(query);
                 if (rs.first()) {
-                    // if user already in the datase, get user level
                     Level = rs.getInt("Level");
                     JdbcConnector.closeDatabase();
                 } else {
-                    //else insert user info to datavase
                     JdbcConnector.closeDatabase();
-//                    query = "INSERT INTO userInfo(Id,Name,Level) VALUES (" + Id +','+Name+",1)";
                     query = "INSERT INTO account VALUES ('" + Id + "', '" + Name + "','" + 1 + "','" + 0 + "')";
                     boolean r1 = JdbcConnector.excuteUpdate(query);
                     if (r1) {
@@ -47,17 +41,11 @@ public class LoginServlet extends HttpServlet {
                     }
                 }
             }
-            // create bean
-            HttpSession session = request.getSession();
-            Bean.UserInfoBean UserInfo = (Bean.UserInfoBean) session.getAttribute("UserInfo");
-            if(UserInfo != null ){
-                
-            }
+            Bean.UserInfoBean UserInfo = new Bean.UserInfoBean();
             UserInfo.setUserId(Id);
             UserInfo.setName(Name);
             UserInfo.setLevel(Level);
-            request.getSession().setAttribute("UserInfo", UserInfo);
-            /* test */
+            request.getSession().setAttribute("UserInfo",UserInfo);
             request.getRequestDispatcher("Game.jsp").forward(request, response);
         }
     }
